dcfa4e29b4de8ed1f47256f60a29f0f3ac5c2d40
kimjinsoo <kimjs@dramancompany.com>
2021-11-29 22:51:24 +0900

    check errors or error message with errors instance method

--- app/models/person.rb
+++ app/models/person.rb
@@ -15,5 +15,15 @@ class Person < ApplicationRecord
   # new라는 것을 할떄는 validates를 지정했을때의 에러를 띄우지는 않는다. 물론 valid를 하면은 false를 리턴하겠지만 말이다.
   # 그 이외에 여러가지가 있긴 한데, 이외의 메소드는 validate를 skip하니까 조심하도록 해야 한다.
   # 또는 save(validate:false) 를 하게 되면은 validate를 하지 않는다는 특징이 있는데, 이런건 조심해서 써야한다.
+
+  # validate에서 error가 생긴다면은 Active Record의 errors라는 method를 통해서
+  # 어떤 것이 error를 발생시켰는지 확인 할 수 있다.
+  # save!, update!, create!를 하지 않는 이상 error를 raise하지는 않는다
+  # 그렇기 때문에 !를 뺀 method를 시도해보고, error가 발생한다면은 ActiveRecord.errors 또는 ActiceRecords.errors.full_messages
+  # 라는 것을 이용해서 어떤 것이 error를 유발했는지 테스트하도록 하자.
+
+  # 관련하여 ActiceRecord.errors.first.full_message
+  # 이런식으로 사용법이 있는걸 보아서는 errors라는 것은 error의 array를 리턴한다고 생각하면 되고
+  # 각각의 full_message를 볼때는 full_message를 사용한다고 생각하면 된다.
   validates :name, presence: true
 end
