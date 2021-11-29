f7bdc9d2b49fb0e3b79d8f20dea5a0bd94b85275
kimjinsoo <kimjs@dramancompany.com>
2021-11-29 23:13:57 +0900

    validates exclusion

--- app/models/library.rb
+++ app/models/library.rb
@@ -9,4 +9,10 @@ class Library < ApplicationRecord
   # book에 library가 참조되어 있지 않으면은 book은 valid 하지 않는 것으로 판별된다.
   # 근데 이것을 쓴 프로젝트는 아직 보지 못했던 것 같은데 나중에 이것을 쓰는지 참조해보자.
   validates_associated :books
+
+  # exclusion은 배타적인 것이고 in이랑 같이 사용된다.
+  # in 안에 있는 것은 제외를 해야 하는 validation이고 다음과 같이 message를 안에다가 넣어줄 수 있다.
+  # 아마 %{value}가 유저가 입력하려고 했던 그 값을 의미하는 것 같다.
+  # 그 값을 rails가 value에 넣어주는 것이라고 생각하면 된다.
+  validates :name, exclusion: { in: %w(hello world), message: "%{value} is reserved." }
 end
