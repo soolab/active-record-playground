14b4687f051cb49ae4f73a4ca6d1a443dd1ecacb
kimjinsoo <kimjs@dramancompany.com>
2021-11-29 12:48:10 +0900

    active-record all, first, last, find_by, where, order

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
 
   # 반면 create는 object를 return 하고
   # db에 해당 object의 정보를 저장한다고 생각하면 된다.
+
+  # 읽는 방법에 대해서 알아보자
+  # User.all 이 있고, User.first, User.last 뭐 이런 간단한 것들이 존재하는 것이고
+  # User.find_by(key:value) 형식으로 주면은 where 절을 비슷하게 구현할 수 있다는 것이다.
+  # User.where(key:value, key:value) 형식으로도 찾을 수 있는데
+  # 찾으면은 보통 array 형식으로 리턴이 나올 텐데
+  # 여기에 .order(key: :desc, :asc)를 하면 정렬을 할 수 있다. order by 를 이렇게 이용하면 될 것 같다.
+  # find_by!를 하고 조건에 맞는 record를 찾지 못한다면은 error를 리턴하게 됨
+  # ActiveRecord::RecordNotFound를 리턴한다는 것을 알아두도록 하자. 꼭 있어야 하는 것일때 이렇게 설정하면 좋을 것 같다.
+  # 그나저나 find_by 와 where의 차이점은 무엇이란 말인가??
 end
