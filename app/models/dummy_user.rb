c039edb67a53daa9952b3d00adeb680e3dcc8eeb
kimjinsoo <kimjs@dramancompany.com>
2021-12-01 22:04:42 +0900

    after_initialized, after_find call back

--- app/models/dummy_user.rb
+++ app/models/dummy_user.rb
@@ -22,6 +22,22 @@ class DummyUser < ApplicationRecord
   #   self.name = login.capitalize if name.blank?
   # end
 
+  # 이런식으로 user가 초기화 될 때 마다 뭔가 호출하도록 하게 할 수 있음.
+  # 이런게 다 call back의 종류라고 생각할 수 있다.
+  after_initialize do |user|
+    puts "#{user.login} you have initialized an object!"
+  end
+
+  # 찾아 질 떄 마다 부루는게 있구나.
+  # 뭔가 찾고나서 후처리를 해야 할 경우 쓰면 괜찮을 것 같다.
+  # 이런것들도 do end가 아니라 method로 만들고 제공하고 싶은데 되려나.
+  # 근데 user가 있네. parameter를 하나 주면 되려나.
+  # 파라미터 하나 주고 하려고 했는데 실패함. yield의 영역이락 그런감.
+  # 나중에 더 찾아봐야 할 것 같다.
+  after_find do |user|
+    puts " #{user.login} You have found an object"
+  end
+
   private
 
   def ensure_login_has_a_value
